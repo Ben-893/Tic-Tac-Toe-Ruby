312ad9c6ecef128ac85775f69fefe0a5f2a10558
Benjamin Smith-Gordon <ben.smith.gordon@gmail.com>
2018-10-17 19:02:35 +0100

    Added a TicTacToe class with a board attr_reader and Initialize method.

---
+++ lib/tictactoe.rb
@@ -0,0 +1,12 @@
+require_relative 'board'
+
+class TicTacToe
+
+  attr_reader :board
+
+  def initialize()
+  @board = board.grid
+  @player = true
+  end
+
+end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,6 +1,6 @@
 require 'tictactoe.rb'
 
-describe '#play' do
+describe '#move' do
   it 'should allow player 1 to place a token in slot 1' do
     ticktactoe.move(0)
     expect(tictactoe.board). to eq([true, nil, nil, nil, nil, nil, nil, nil, nil])
