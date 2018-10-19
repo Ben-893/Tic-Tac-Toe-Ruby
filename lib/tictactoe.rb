140053acdc67054cc287a7fdc56a99898a0ba210
Benjamin Smith-Gordon <ben.smith.gordon@gmail.com>
2018-10-19 20:12:47 +0100

    Changed around some code.

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,19 +1,21 @@
 require_relative 'board'
 
 class TicTacToe
+  WinConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
   attr_reader :board
 
   def initialize
-  board=Board.new
-  @board = board.grid
-  @player = true
+    board=Board.new
+    @board = board.grid
+    @player = true
   end
 
   def move(slot)
-    raise 'Not a valid number' unless slot.is_a? Integer
-    raise 'Invalid Slot' if slot > @board.size - 1
-    raise 'Slot is in use' if @board[slot] != nil
-    @board[slot] = @player
+    raise 'Not a valid number' unless slot =~ /^-?[0-9]+$/
+    new_slot = slot.to_i
+    raise 'Invalid Slot' if new_slot > @board.size - 1
+    raise 'Slot is in use' if @board[new_slot] != nil
+    @board[new_slot] = @player
   end
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -4,18 +4,18 @@ describe TicTacToe do
 
   describe '#move' do
     it 'should allow player 1 to occupy slot 0' do
-      subject.move(0)
+      subject.move('0')
       expect(subject.board).to eq([true, nil, nil, nil, nil, nil, nil, nil, nil])
       end
     end
     it 'should raise an error if a player tries to use a slot thats already taken' do
-      subject.move(0)
-      expect{subject.move(0)}.to raise_error('Slot is in use')
+      subject.move('0')
+      expect{subject.move('0')}.to raise_error('Slot is in use')
     end
     it 'should raise an error if a player tries to select a slot greater than 9' do
-      expect{subject.move(9)}.to raise_error('Invalid Slot')
+      expect{subject.move('9')}.to raise_error('Invalid Slot')
     end
     it 'should raise an error if the player tries to enter a character that is not an integer' do
-      expect{subject.move("$")}.to raise_error('Not a valid number')
+      expect{subject.move('$')}.to raise_error('Not a valid number')
     end
   end
