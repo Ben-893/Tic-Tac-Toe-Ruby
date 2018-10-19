aa24b47397b02b41698e1cf8d719ea8e587f1dbb
Benjamin Smith-Gordon <ben.smith.gordon@gmail.com>
2018-10-19 12:56:47 +0100

    Created a test that will raise an error if a player tries to occupy a slot that's already in use.

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -3,9 +3,13 @@ require 'tictactoe.rb'
 describe TicTacToe do
 
   describe '#move' do
-    it 'should allow player 1 to place a token in slot 1' do
+    it 'should allow player 1 to occupy slot 0' do
       subject.move(0)
       expect(subject.board).to eq([true, nil, nil, nil, nil, nil, nil, nil, nil])
       end
+    end
+    it 'should raise an error if a player tries to use a slot thats already taken' do
+      subject.move(0)
+      expect{subject.move(0)}.to raise_error('Slot is in use')
     end
   end
