01e86cb44d3d91eeda960f75cc1054b736686d8b
Benjamin <ben.smith.gordon@gmail.com>
2018-12-18 18:56:46 +0000

    More tests

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ gem 'rspec'
 gem 'rubocop'
 gem 'simplecov', require: false, group: :test
 gem 'simplecov-console', require: false
+gem 'pry'
+gem 'pry-nav'
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -10,6 +10,9 @@ describe TicTacToe do
     it 'should start with player x as the current player' do
       expect(subject.current_player.piece).to eq('x')
     end
+    it 'should place a piece in a slot' do
+      expect(subject.move('0')).to eq 'x'
+    end
   end
   
   describe '#slot_avalible?' do
@@ -19,6 +22,19 @@ describe TicTacToe do
     end
     it 'should return false if a player tries to select a slot greater than 9' do
       expect(subject.slot_avalible?('9')).to eq false
+    end
+  end
+
+  describe '#valid?' do
+    it 'should return true if the character is valid' do
+      expect(subject.valid?('0')).to eq true
+    end
+    it 'should return nil if the character is not valid' do
+      expect(subject.valid?(0)).to eq nil
+    end
+    it 'should return false if the slot if already taken' do
+      allow(subject).to receive(:slot_avalible?).and_return(false)
+      expect(subject.valid?('0')).to eq false
     end
   end
 
@@ -32,6 +48,11 @@ describe TicTacToe do
     it 'should change the next players turn' do
       subject.switch_player
       expect(subject.current_player.piece).to eq('o')
+    end
+    it 'should change the next players turn to x' do
+      subject.switch_player
+      subject.switch_player
+      expect(subject.current_player.piece).to eq('x')
     end
   end
 
